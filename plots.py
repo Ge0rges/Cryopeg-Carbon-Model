228acd71119d710d94ea69291994847fd7978798
Georges Kanaan <ge0rges@ge0rges.com>
2022-08-02 14:59:10 -0700

    better rapid scenario

--- plots.py
+++ plots.py
@@ -75,13 +75,13 @@ def hypothetical_growth_scenarios():
 
     fig.suptitle("Hypothetical growth scenarios", fontsize="x-large", fontweight="medium")
 
-    x = np.linspace(0, 40000, num=40000)
+    x = np.linspace(0, 40000, num=4000000)
 
     x_exp = np.linspace(0, 6.90776, num=len(x))
     y_exp = 10**5 * np.exp(x_exp)
     y_cyclic = 10**5 + (10**8 - 10**5)/2 + np.sin(x/4244.1333333333 + 1.5*np.pi) * (10**8 - 10**5)/2
     y_ng = np.full(shape=len(x), fill_value=10**8)
-    y_spike = np.where(x < 10, x*10**7 + 10**5, 10**8)
+    y_spike = np.where(x < 0.02, 10**5, np.where(x < 10, x*10**7 + 10*5, 10**8))
 
     axis.loglog(x, y_spike, label='Rapid', color="blue")
     axis.loglog(x, y_exp, label='Slow', color="green")
